cd1989e31689cf42f688561c770b8913ededd9f3
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-08 15:00:46 -0800

    Time: 0 ms (100.00%), Space: 5.8 MB (92.73%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.cpp
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
-        int i = 0, j = 0;
-        int n = word.size(), m = abbr.size();
-        while (i < n && j < m) {
-            if (isdigit(abbr[j])) {
+        int i=0;
+        int j=0;
+        int ws = word.size();
+        int as = abbr.size();
+        while(i < ws && j < as) {
+            if(isdigit(abbr[j])) {
                 if (abbr[j] == '0') return false;
-                int k = 0;
-                while(j < m && isdigit(abbr[j]))
-                    k = k*10 + abbr[j++] - '0';
-                i += k;
+                int val = 0;
+                while(isdigit(abbr[j]) && j < as) {
+                    val = (val * 10 + abbr[j++]-'0');
+                }
+                i+=val;
             } else {
-                if (word[i++] != abbr[j++])
+                if(word[i++] != abbr[j++]) {
                     return false;
+                }
             }
         }
-        
-        return i == n && j == m;
+        return i == ws && j == as;
     }
-    
 };
