9bbf828605cb7d0c7757cbd0f2b63353b71318af
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-08 17:57:12 -0800

    Time: 7 ms (18.59%), Space: 6 MB (76.40%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -7,45 +7,53 @@ public:
         int plmi = 0;
         int slen = s.length();
         
+        //digit, dot, e, +-
+        
         for(int i=0;i<slen;i++) {
-            cout << s[i] << endl;
             if(s[i] >= '0' && s[i] <= '9') {
-                digit = true;
-            } else if(s[i] == '+' || s[i] == '-') {
-                //1.there can be only 1 + or -
-                //2.can't occur at the end
-                //3.it can only occur after e OR starting of the string
-                if(plmi > 1) {
-                    return false;
-                }
-                if(i > 0 && (s[i-1] != 'e' && s[i-1] != 'E')) {
-                    return false;
-                }
-                if(i == slen-1) {
-                    return false;
-                }
-                plmi++;
-            } else if(s[i] == '.') {
-                //1.can't contain miltiple dots
-                //2.dot can not occurs after e
-                //3.dot can't be last character of the string
-                //4.if there are no digits then also false
-                if(dot == true || e == true) {
+                digit=true;
+            } else if(s[i]=='.') {
+                //1. .can't occur after e
+                //2.only one dot allowed
+                //3.there should be atlease on digit till the end of the string
+                if(dot || e) {
                     return false;
                 }
                 if(i == slen-1 && !digit) {
-                    cout << "x";
                     return false;
                 }
+                
                 dot = true;
             } else if(s[i] == 'e' || s[i] == 'E') {
-                //1.multiple e's not allowed
-                //2.e can't occur at the end
-                //3. e9 OR .e1 - if e occured then there should atleaset one digit before
-                if(e || !digit || i == s.length()-1){
+                //1.e can't be starting point
+                //2.only single e is allowed
+                //3.e should occur after one digit
+                // .e e33
+                if(e || i == 0) {
+                    cout << "aa";
+                    return false;
+                }
+                if(i == slen-1 || !digit) {
+                    cout << "a";
                     return false;
                 }
                 e = true;
+            } else if(s[i] == '+' || s[i] == '-') {
+                //1.there are only one + or - allowed
+                //2.can't be at the end
+                //3.if occurs in the middle then before that must an e or E
+                if(plmi > 1 || i == slen-1) {
+                    cout << "aaa";
+                    return false;
+                }
+                if(i > 0) {
+                    if(s[i-1] != 'e' && s[i-1] != 'E') {
+                        cout << "aaaa";
+                        return false;
+                    }
+                }
+                
+                plmi++;
             } else {
                 return false;
             }
