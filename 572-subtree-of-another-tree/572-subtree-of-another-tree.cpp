2c87e37f32814e6e1fb2175c491d3f8feb9b7928
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-13 16:15:27 -0800

    Time: 20 ms (82.89%), Space: 28.9 MB (21.97%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -17,8 +17,11 @@ public:
         return root->val==subRoot->val && helper(root->left,subRoot->left) && helper(root->right,subRoot->right);
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root==NULL) {
-            return subRoot==NULL;
+        if(root==NULL && subRoot==NULL) {
+            return true;
+        }
+        if(root==NULL || subRoot==NULL) {
+            return false;
         }
         return helper(root,subRoot) || isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
