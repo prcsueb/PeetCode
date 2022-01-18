ddac21461952258ee1e251af05596d7f04743cad
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-17 23:10:17 -0800

    Time: 358 ms (9.01%), Space: 97.3 MB (16.25%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        map<int,int> mp; //{prefixsum%k , idx}
-        int n =nums.size();
-        mp[0]=-1;
+        int n=nums.size();
+        map<int,int> mp; //{prefixsum%k,index}
         int sum=0;
-        for(int i=0;i<n;i++) {
-            sum += nums[i];
+        mp[0]=-1;
+        //(sum1-sum2)%k==0.
+        //sum1%k=sum2%k return true and size of subarray is >= 2
+        for(int i=0;i<nums.size();i++) {
+            sum+=nums[i];
             if(mp.find(sum%k)!=mp.end()) {
-                //checking subarray size is >=2 only then it's valid
-                if(i - mp[sum%k] >= 2) {
+                if(i-mp[sum%k]>=2) {
                     return true;
                 }
             }
             else {
                 mp[sum%k]=i;
             }
-            
         }
-        
         return false;
     }
 };
