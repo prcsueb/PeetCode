8c3ba744e6cc743d3906c3e6bb7f07be09e88a94
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-18 02:22:36 -0800

    Time: 24 ms (65.08%), Space: 13.4 MB (51.66%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.cpp
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    struct Log{
+    struct Log {
         int id;
         int start;
         int child;
     };
     vector<int> exclusiveTime(int n, vector<string>& logs) {
-        vector<int> ans(n);
         stack<Log> stk;
+        vector<int> ans(n);
         for(int i=0;i<logs.size();i++) {
-            stringstream ss(logs[i]);
-            //convert stream and extract startTime,status,endTIme
+            string log = logs[i];
+            stringstream ss(log);
             string id,status,startTime;
             getline(ss,id,':');
             getline(ss,status,':');
             getline(ss,startTime,':');
-            
-            //create strucutre out of it
             Log l = {stoi(id),stoi(startTime),0};
             if(status == "start") {
                 stk.push(l);
             } else {
-                //if it's ending time then calculate interval with and without interval
-                //with child will be pushed to ans vector we add not override answer
-                //without child will be pushed to child of top of the stack
-                int intervalWithChild = stoi(startTime) - stk.top().start - stk.top().child + 1;
-                int interval = stoi(startTime) - stk.top().start + 1;
-                ans[stk.top().id] += intervalWithChild;
+                int intervalWithOutChild = stoi(startTime)-stk.top().start+1;
+                int intervalWithChild = stoi(startTime)-stk.top().start-stk.top().child+1;
+                ans[stk.top().id]+=intervalWithChild;
                 stk.pop();
                 if(!stk.empty()) {
-                    stk.top().child += interval;
+                    stk.top().child+=intervalWithOutChild;
                 }
             }
         }
