41da24ee419497274fb1c8cab4424ae04b5badb0
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-18 17:20:38 -0800

    Time: 301 ms (31.10%), Space: 92.5 MB (57.55%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.cpp
+++ 1891-cutting-ribbons/1891-cutting-ribbons.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int maxLength(vector<int>& ribbons, int k) {
-
-        long long sum = 0;
-        for(int i=0;i<ribbons.size();i++) {
-            sum += ribbons[i];
+    int checkAns(vector<int>& ribbons, int mid) {
+        int count=0;
+        for(auto x : ribbons) {
+            count+=(x/mid);
         }
-        if(sum<k)return 0;
-        long long low = 1, high = sum;
-        while(low <= high){
-            long long mid = low + (high-low) / 2;
-            long long count = totalPeice(ribbons, k , mid);
-            if(count < k){
-                high = mid - 1;
-            }else{
-                low= mid + 1;
+        return count;
+    }
+    int maxLength(vector<int>& ribbons, int k) {
+        long long n=ribbons.size();
+        long long low=1;
+        long long sum=0;
+        for(long long i=0;i<n;i++) {
+            sum+=ribbons[i];
+        }
+        
+        long long high=sum;
+        while(low<=high) {
+            long long mid=low+(high-low)/2;
+            long long count = checkAns(ribbons,mid);
+            if(count<k) {
+                high=mid-1;
+            } else {
+                low=mid+1;
             }
         }
         return high;
-    }
-    int totalPeice(vector<int>& ribbons, int k, int value){
-        int count = 0;
-        for(int ele : ribbons){
-            count += ele / value;
-        }
-        return count;
     }
 };
