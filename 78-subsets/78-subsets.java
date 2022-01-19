54acc095f5ac76626c699045d39a00a9795ada7a
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-19 05:30:26 -0800

    Time: 2 ms (36.34%), Space: 39.7 MB (42.69%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    //Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        if(start == nums.length){
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+        tempList.add(nums[start]);
+        backtrack(list, tempList, nums, start + 1);
+        tempList.remove(tempList.size() - 1);
+        backtrack(list, tempList, nums, start + 1);
+    }
+}
