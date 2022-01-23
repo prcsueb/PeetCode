6303544391d4faeabb4ee60f2b211bd8ee7c42f1
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-01-22 16:45:58 -0800

    Time: 7 ms (76.88%), Space: 10.8 MB (89.14%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     void helper(vector<int>& can, int target, int idx, vector<int>& sub,vector<vector<int>> &ans) {
-        if(can.size() == idx) {
-            if(target==0) {
-                ans.push_back(sub);
-            }
+        if(target==0) {
+            ans.push_back(sub);
             return;
-        }    
-        if(can[idx] <= target) {
-            sub.push_back(can[idx]);
-            helper(can,target-can[idx],idx,sub,ans);
-            sub.pop_back();
+        }   
+        // if(can[idx] <= target) {
+        //     sub.push_back(can[idx]);
+        //     helper(can,target-can[idx],idx,sub,ans);
+        //     sub.pop_back();
+        // }
+        // helper(can,target,idx+1,sub,ans);
+        for(int i=idx;i<can.size();i++) {
+            if(can[i] <= target) {
+                sub.push_back(can[i]);
+                helper(can,target-can[i],i,sub,ans);
+                sub.pop_back();
+            }
         }
-        helper(can,target,idx+1,sub,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> sub;
