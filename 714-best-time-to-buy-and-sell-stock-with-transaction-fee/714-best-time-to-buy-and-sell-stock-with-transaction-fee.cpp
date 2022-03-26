fee733189c8aa7565fed394cc8ac96ee51585309
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-03-26 16:44:49 -0700

    Time: 96 ms (90.87%), Space: 54.9 MB (87.83%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int oldBuyingStateProfit = -prices[0];
-        int oldSellingStateProfit=0;
-        
+        int bsp = -prices[0], ssp = 0;
         for(int i=1;i<prices.size();i++) {
-            int newBuyingStateProfit=0;
-            int newSellingStateProfit=0;
-            if(oldBuyingStateProfit > oldSellingStateProfit-prices[i]) {
-                newBuyingStateProfit = oldBuyingStateProfit;
-            } else {
-                newBuyingStateProfit = oldSellingStateProfit-prices[i];
-            }
-            
-            if(oldSellingStateProfit > oldBuyingStateProfit+prices[i]-fee) {
-                newSellingStateProfit = oldSellingStateProfit;
-            } else {
-                newSellingStateProfit = oldBuyingStateProfit+prices[i]-fee;
-            }
-            oldBuyingStateProfit = newBuyingStateProfit;
-            oldSellingStateProfit = newSellingStateProfit;
-            
+            bsp = max(bsp, ssp - prices[i]);
+            ssp = max(ssp, prices[i] + bsp - fee);
         }
-        
-        return oldSellingStateProfit;
+        return ssp;
     }
 };
