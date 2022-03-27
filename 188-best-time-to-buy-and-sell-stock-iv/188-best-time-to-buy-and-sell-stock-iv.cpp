cee00ab75efb01872d3781ca85381683281c55bf
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-03-27 15:39:07 -0700

    Time: 1290 ms (5.01%), Space: 11.6 MB (61.26%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,15 +6,16 @@ public:
         if(k==0) return 0;
         vector<vector<int>> dp(k+1, vector<int> (n, 0));
         
-        for(int i=1;i<=k;i++) {
-            for(int j=1;j<n;j++) {
-                int maxVal = dp[i][j-1];
-                for(int k=0;k<j;k++) {
-                    dp[i][j] = max(maxVal, dp[i-1][k] + (prices[j] - prices[k]));
-                    maxVal = max(maxVal, dp[i][j]);
+        for(int t=1;t<=k;t++) {
+            for(int d=1;d<n;d++) {
+                int maxx = dp[t][d-1];
+                for(int prevday = 0; prevday < d; prevday++) {
+                    int pastDayTm1 = dp[t-1][prevday];
+                    int tthproft = prices[d] - prices[prevday];
+                    maxx = max(maxx, pastDayTm1+tthproft);
                 }
+                dp[t][d] = maxx;
             }
-            cout << "\n";
         }
         
         return dp[k][n-1];
