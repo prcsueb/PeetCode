1c3a03ebaefb81a072d80f24a2e0baaa1e9cb951
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-03-28 16:22:12 -0700

    Time: 456 ms (21.11%), Space: 10.6 MB (39.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -6,13 +6,12 @@ public:
         dp[0]=1;
         int ans=1;
         for(int i=1;i<n;i++) {
-            int maxx=0;
             for(int j=i-1;j>=0;j--) {
                 if(nums[i] > nums[j]) {
-                    maxx = max(maxx, dp[j]);
+                    dp[i] = max(dp[i], dp[j]);
                 }
             }
-            dp[i] = maxx + 1;
+            dp[i]++;
             ans = max(ans, dp[i]);
         }
         
