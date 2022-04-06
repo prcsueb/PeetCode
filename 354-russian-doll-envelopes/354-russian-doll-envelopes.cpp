342cc1b9d1972299c8e5c718b39a570d90e2ac9f
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-04-06 15:30:48 -0700

    Time: 560 ms (37.75%), Space: 87.8 MB (18.24%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -3,12 +3,15 @@ public:
     static bool comparefn(vector<int> &a, vector<int> &b)
     {
         if(a[0] == b[0])
-            return a[1] > b[1];
+            return a[1] > b[1]; //desc base on height becase if there is a case prevent calculating the envelope with the same width? For example [3, 1] [3, 2] [3, 3] will get 3, but [3, 3], [3, 2], [3, 1] will get 1
         return a[0] < b[0];
     }
     int maxEnvelopes(vector<vector<int>>& intervals) {
         int n = intervals.size();
         sort(intervals.begin(), intervals.end(), comparefn);
+        for(auto x : intervals) {
+            cout << x[0] << "," << x[1] << endl;
+        }
         vector<int> dp;
         
         for(int i=0;i<n;i++) {
