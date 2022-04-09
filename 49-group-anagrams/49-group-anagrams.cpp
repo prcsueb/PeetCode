5177506a532c63cb921f1cf02d4ee78004033c4b
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-04-09 10:27:58 -0700

    Time: 53 ms (50.48%), Space: 20.7 MB (50.25%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void processString(string s, map<map<char,int>,vector<string>> &mp) {
-        int slen = s.length();
-        map<char,int> frequeMp;
-        for(int i=0;i<slen;i++) {
-            frequeMp[s[i]]++;
-        }
-        mp[frequeMp].push_back(s);
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(auto s : strs) {
+            string st = s;
+            sort(st.begin(), st.end());
+            mp[st].push_back(s);
+        }
+        
         vector<vector<string>> ans;
-        map<map<char,int>,vector<string>> mp;
-        for(auto x : strs) {
-            //process and fill map
-            processString(x, mp);
-        }
-        //fill ans using map
         for(auto it=mp.begin();it!=mp.end();it++) {
-            ans.push_back(it->second);
+            vector<string> intermediate;
+            for(auto y : it->second) {
+                intermediate.push_back(y);
+            }
+            ans.push_back(intermediate);
         }
-        //return answer
+        
         return ans;
     }
 };
