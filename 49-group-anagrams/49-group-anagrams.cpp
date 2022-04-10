9611bd13bd8740dfe0d128b63b7092d66be9c2aa
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-04-09 21:05:08 -0700

    Time: 272 ms (5.03%), Space: 254.1 MB (5.01%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
+    string strSort(string s) {
+        string str="";
+        vector<int> freq(26, 0);
+        for(int i=0;i<s.length();i++) {
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<freq.size();i++) {
+            str += string(i + 'a', freq[i]);
+        }
+        return str;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
+        
         for(auto s : strs) {
-            string st = s;
-            sort(st.begin(), st.end());
-            mp[st].push_back(s);
+            mp[strSort(s)].push_back(s);
         }
         
         vector<vector<string>> ans;
