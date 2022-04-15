7994c116b12e5da892ffe0451bb297bf9ba2d736
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-04-15 11:16:56 -0700

    Time: 3 ms (97.90%), Space: 11.7 MB (39.60%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        int skip = 0;
+        int prevSkip;
+        int took = nums[0];
+        for(int i=1;i<n;i++) {
+            prevSkip = skip;
+            skip = max(skip, took);
+            took = nums[i] + prevSkip;
+        }
+        return max(skip, took);
+    }
+    int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        int maxx = -1;
+        for(auto &x : nums) {
+            maxx = max(maxx, x);
+        }
+        vector<int> nums2(maxx+1, 0);
+        for(auto &x : nums) {
+            nums2[x] += x;
+        }
+        
+        return rob(nums2);
+    }
+};
