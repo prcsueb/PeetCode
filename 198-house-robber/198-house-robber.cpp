30e534253e7deeab5539ac480b6d87f9c3be15da
prcsueb <76527499+prcsueb@users.noreply.github.com>
2022-04-15 07:09:51 -0700

    Time: 2 ms (53.55%), Space: 7.7 MB (47.80%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int helper(vector<int>& nums, int idx, vector<int> &dp) {
+        if(idx >= nums.size()) {
+            return 0;
+        }
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
+        int case1 = helper(nums, idx+1, dp);
+        int case2 = helper(nums, idx+2, dp) + nums[idx];
+        return dp[idx] = max(case1, case2);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(100, -1);
+        return helper(nums, 0, dp);
+    }
+};
