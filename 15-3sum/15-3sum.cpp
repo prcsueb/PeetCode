a3133322d7880eedb1368222338b65f9853a940d
Premang Raithatha <76527499+prcsueb@users.noreply.github.com>
2022-05-13 19:16:39 -0700

    Time: 137 ms (45.45%), Space: 19.8 MB (96.40%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         if(nums.size()<3)return {};
+        sort(nums.begin(), nums.end());
+        int i=0;
+        int n = nums.size();
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        
-        //a+b+c=0;
-        //b+c=-a; -a is -sum here
-        for(int i=0;i<nums.size()-2;i++) {
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])) {
-                int sum=-nums[i];
-                int low=i+1;
-                int high=nums.size()-1;
-                while(low<high) {
-                    if(sum == nums[low]+nums[high]) {
-                        ans.push_back({nums[i],nums[low],nums[high]});
-                        while(low<high && nums[low] == nums[low+1]) {
+        while(i < n) {
+            if(i == 0 || nums[i] != nums[i-1]) {
+                int a = nums[i];
+                int low = i+1;
+                int high = n-1;
+                while(low < high) {
+                    int sum = nums[low] + nums[high];
+                    if(sum == -a) {
+                        ans.push_back({a, nums[low], nums[high]});
+                        while(low < high && nums[low] == nums[low+1]) {
                             low++;
                         }
-                        while(low<high && nums[high] == nums[high-1]) {
+                        while(high > low && nums[high] == nums[high-1]) {
                             high--;
                         }
-                        low++;high--;
-                    } else if((nums[low]+nums[high]) < sum) {
+                        low++;
+                        high--;
+                    } else if(sum < -a) {
                         low++;
                     } else {
                         high--;
                     }
-                }   
+                }
             }
+            i++;
         }
         return ans;
     }
