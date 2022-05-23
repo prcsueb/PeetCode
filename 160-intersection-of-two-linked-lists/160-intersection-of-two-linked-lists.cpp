d5cc461461433cec7ab9653c47b5b4bebd486fd2
Premang Raithatha <76527499+prcsueb@users.noreply.github.com>
2022-05-23 13:04:55 -0700

    Time: 49 ms (81.39%), Space: 14.6 MB (73.69%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,43 +8,36 @@
  */
 class Solution {
 public:
+    int getLen(ListNode *x) {
+        int len = 0;
+        while(x != NULL) {
+            x=x->next;
+            ++len;
+        }
+        return len;
+    }
+    ListNode *moveByDist(ListNode *x, int dist) {
+        while(dist--) {
+            x=x->next;
+        }
+        return x;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *slow=headA;
-        ListNode *fast=headB;
-        int len1 = 0;
-        int len2 = 0;
-        while(slow!=NULL) {
-            slow = slow->next;len1++;
-        }
-        while(fast!=NULL) {
-            fast = fast->next;len2++;
-        }
-        int diff = abs(len1-len2);
-        slow=headA;
-        fast=headB;
-        if(len1>len2) {
-            while(diff--) {
-                slow=slow->next;
-            }
-            while(slow!=fast && slow!=NULL && fast!=NULL){
-                slow=slow->next;
-                fast=fast->next;
-            }
-        } else if(len2>len1) {
-            while(diff--) {
-                fast=fast->next;
-            }
-            while(slow!=fast && slow!=NULL && fast!=NULL){
-                slow=slow->next;
-                fast=fast->next;
-            }
-        } else {
-            while(slow!=fast && slow!=NULL && fast!=NULL){
-                slow=slow->next;
-                fast=fast->next;
-            }
+        int aLen = getLen(headA);
+        int bLen = getLen(headB);
+        if(aLen > bLen) {
+            headA = moveByDist(headA, aLen-bLen);
+        } else if(bLen > aLen) {
+            headB = moveByDist(headB, bLen-aLen);
         }
         
-        return fast==slow? slow:NULL;
+        while(headA!=NULL && headB!=NULL) {
+            if(headA==headB) {
+                return headA;
+            }
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return NULL;
     }
 };
