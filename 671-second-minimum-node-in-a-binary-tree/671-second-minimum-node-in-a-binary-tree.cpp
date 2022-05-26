b887869e05e950c818d4ebe7b0a0734ea2d440d0
Premang Raithatha <76527499+prcsueb@users.noreply.github.com>
2022-05-26 13:29:23 -0700

    Time: 0 ms (100.00%), Space: 7.2 MB (26.24%) - LeetHub

--- 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.cpp
@@ -11,76 +11,19 @@
  */
 class Solution {
 public:
-    class Node {
-        public:
-        int val;
-        Node *prev = NULL;
-        Node *next = NULL;
-        Node(int v) {
-            val = v;
-        }
-    };
-    
-    Node *head = new Node(-1);
-    Node *tail = new Node(-1);
-    
-    int noOfNodes() {
-        if(head->next->val == -1) return 0;
-        else if(head->next == tail->prev) return 1;
-        else if(head->next != tail->prev) return 2;
-        return 0;
-    }
-    
-    void insertNext(Node *node, Node *newNode) {
-        node->next->prev = newNode;
-        newNode->next = node->next;
-        newNode->prev = node;
-        node->next = newNode;
-    }
-    
-    void insertPrev(Node *node, Node *newNode) {
-        node->prev->next=newNode;
-        newNode->prev=node->prev;
-        newNode->next=node;
-        node->prev = newNode;
-    }
-    
-    void helper(TreeNode *root) {
-        if(!root) return;
-        int count = noOfNodes();
-        Node *newNode = new Node(root->val);
-        if(count == 0) {
-            insertNext(head, newNode);
-        } else if(count == 1 && head->next->val != root->val) {
-            if(root->val < head->next->val) {
-                insertNext(head,newNode);
-            } else if(root->val > head->next->val) {
-                insertPrev(tail,newNode);
-            }
-        } else if(count == 2 && head->next->val != root->val && tail->prev->val != root->val) {
-            if(root->val < head->next->val) {
-                head->next->next->val = head->next->val;
-                head->next->val=root->val;
-            } else if(root->val < tail->prev->val) {
-                tail->prev->val = root->val;
-            }
-        }
-        helper(root->left);
-        helper(root->right);
-    }
-    
     int findSecondMinimumValue(TreeNode* root) {
+        if(root==NULL) return -1;
         if(!root->left && !root->right) return -1;
-        
-
-        head->next = tail;
-        tail->prev = head;
-        
-        helper(root);
-        int count = noOfNodes();
-        if(count == 0 || count == 1) {
-            return -1;
+        int left = root->left->val;
+        int right = root->right->val;
+        if(left == root->val) {
+            left = findSecondMinimumValue(root->left);
         }
-        return tail->prev->val;
+        if(right == root->val) {
+            right = findSecondMinimumValue(root->right);
+        }
+        if(left!=-1 && right!=-1) return min(left,right);
+        else if(left!=-1) return left;
+        return right;
     }
 };
