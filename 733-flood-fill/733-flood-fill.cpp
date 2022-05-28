ab72122c839e7c45157530206b300c83c69dfca4
Premang Raithatha <76527499+prcsueb@users.noreply.github.com>
2022-05-28 14:32:33 -0700

    Time: 16 ms (34.02%), Space: 14.2 MB (28.05%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0} ,{0, 1}};
-    void helper(vector<vector<int>>& image, int target, int sr, int sc, int newColor) {
+    vector<vector<int>> dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    void helper(vector<vector<int>>& image, int sr, int sc, int newColor, int sameColor) {
         image[sr][sc]=newColor;
-        for(int i=0;i<dir.size();i++) {
-            int newRow = sr + dir[i][0];
-            int newCol = sc + dir[i][1];
-
-            if(newRow >=0 && newCol >= 0 && newRow < image.size() && newCol < image[0].size() && image[newRow][newCol] == target) {
-                helper(image, target, newRow, newCol, newColor);
+        for(auto d: dir) {
+            int r = sr + d[0];
+            int c = sc + d[1];
+            if(r >= 0 && c >= 0 && r < image.size() && c < image[0].size() && image[r][c] == sameColor) {
+                helper(image,r,c,newColor,sameColor);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         if(image[sr][sc] != newColor) {
-            helper(image, image[sr][sc], sr, sc, newColor);
+            helper(image, sr, sc, newColor, image[sr][sc]);
         }
         return image;
     }
