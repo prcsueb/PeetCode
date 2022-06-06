0ca74836e0cae71d14e1d399ad4cedc0207341db
Premang Raithatha <76527499+prcsueb@users.noreply.github.com>
2022-06-06 03:26:05 -0700

    Time: 737 ms (8.14%), Space: 102.3 MB (21.00%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool dfs(int i, int j, vector<vector<int>> &grid1, vector<vector<int>> &grid2, vector<vector<int>> &dir) {
-        bool ans = grid2[i][j] == grid1[i][j];
-        grid2[i][j]=0;
-        for(auto d : dir) {
-            int newX = i + d[0];
-            int newY = j + d[1];
-            if(newX >= 0 && newY >= 0 && newX < grid2.size() && newY<grid2[0].size() && grid2[newX][newY] == 1) {
-                ans = dfs(newX, newY, grid1, grid2, dir) && ans;
-            } 
+    vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int sr, int sc) {
+        bool ans = grid2[sr][sc] == grid1[sr][sc];
+        grid2[sr][sc] = 0;
+        
+        for(auto &d : dir) {
+            int newRow = sr + d[0];
+            int newCol = sc + d[1];
+            if(newRow >= 0 && newCol >= 0 && newRow < grid2.size() && newCol < grid2[0].size() && grid2[newRow][newCol] == 1) {
+                ans = dfs(grid1, grid2, newRow, newCol) && ans;
+            }
         }
         return ans;
     }
-    
-    
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        int counter=0;
+        int count = 0;
         for(int i=0;i<grid2.size();i++) {
             for(int j=0;j<grid2[0].size();j++) {
                 if(grid2[i][j] == 1) {
-                    bool comp = dfs(i, j, grid1, grid2, dir);
-                    if(comp) counter++;
+                    if(dfs(grid1, grid2, i, j)) {
+                        count++;
+                    }
                 }
             }
         }
-        return counter;
+        return count;
     }
 };
